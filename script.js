d7d950b60ee72ded4f7248ceff1733f1288802c4
shamizen <dev.shamizen@gmail.com>
2021-06-26 22:09:45 +0700

    Edit comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function displayAnswer(id) {
   let element = document.getElementById(id);
   let parent_element = element.parentElement;
   if (parent_element.className === 'faq') {
-    setAllClassName(parent_element, 'faq'); //set all class name to 'faq' | comment this function if you want to show more than 1 answer
+    setAllClassName(parent_element, 'faq'); //set all class name to 'faq' | comment this line if you want to show more than 1 answer
     parent_element.classList.add('display-answer');
   } else if (parent_element.className === 'faq display-answer') {
     parent_element.className = 'faq';
