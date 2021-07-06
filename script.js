6132b4e7ad6f1a37d71b2e487b400421650dee3d
shamizen <dev.shamizen@gmail.com>
2021-07-07 01:33:03 +0700

    Create function addEventOnClick

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function displayAnswer() {
-  let element = this
-  let parent_element = element.parentElement
+  let parent_element = this.parentElement;
   if (parent_element.className === 'faq') {
-    setAllClassName(parent_element, 'faq') //set all class name to 'faq'
-    parent_element.classList.add('display-answer')
+    setAllClassName(parent_element, 'faq'); //set all class name to 'faq'
+    parent_element.classList.add('display-answer');
   } else if (parent_element.className === 'faq display-answer') {
-    parent_element.className = 'faq'
+    parent_element.className = 'faq';
   }
 }
 
 function setAllClassName(element, className) {
   for (let index = 0; index < element.parentElement.childElementCount; index++) {
-    element.parentElement.children[index].className = className
+    element.parentElement.children[index].className = className;
   }
 }
 
-let elements = document.getElementsByClassName('question-container')
-let elementArray = [...elements]
+function addEventOnClick() {
+  let elements = document.getElementsByClassName('question-container');
+  let elementArray = [...elements];
 
-elementArray.forEach((element) => {
-  element.addEventListener('click', displayAnswer, false)
-})
-
-function sayHello() {
-  console.log('Hello')
+  elementArray.forEach((element) => {
+    element.addEventListener('click', displayAnswer, false);
+  });
 }
+
+addEventOnClick();
