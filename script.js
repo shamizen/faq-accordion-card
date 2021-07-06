3df21cd50010b53e1fb48223ed02a30f68ac3f54
shamizen <dev.shamizen@gmail.com>
2021-07-07 01:26:52 +0700

    Reformat code

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function displayAnswer() {
-  let element = this;
-  let parent_element = element.parentElement;
+  let element = this
+  let parent_element = element.parentElement
   if (parent_element.className === 'faq') {
-    setAllClassName(parent_element, 'faq'); //set all class name to 'faq' | comment this line if you want to show more than 1 answer
-    parent_element.classList.add('display-answer');
+    setAllClassName(parent_element, 'faq') //set all class name to 'faq'
+    parent_element.classList.add('display-answer')
   } else if (parent_element.className === 'faq display-answer') {
-    parent_element.className = 'faq';
+    parent_element.className = 'faq'
   }
 }
 
 function setAllClassName(element, className) {
   for (let index = 0; index < element.parentElement.childElementCount; index++) {
-    element.parentElement.children[index].className = className;
+    element.parentElement.children[index].className = className
   }
 }
 
-let elements = document.getElementsByClassName('question-container');
-let elementArray = [...elements];
+let elements = document.getElementsByClassName('question-container')
+let elementArray = [...elements]
 
 elementArray.forEach((element) => {
-  element.addEventListener('click', displayAnswer, false);
-});
+  element.addEventListener('click', displayAnswer, false)
+})
 
 function sayHello() {
-  console.log('Hello');
+  console.log('Hello')
 }
